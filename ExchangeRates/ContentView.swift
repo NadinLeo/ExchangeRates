c5850f46bdbcffc6753ad8e6787a79db5a0ddf62
Nadzeya <Nadezhda.Leonovich@gmail.com>
2019-11-08 12:58:10 +0300

    Initial Commit

---
+++ ExchangeRates/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ExchangeRates
+//
+//  Created by Nadzeya Leanovich on 11/8/19.
+//  Copyright © 2019 Nadzeya Leanovich. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
